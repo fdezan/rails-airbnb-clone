f7b28944750b3b2ceebc4370c2770b6826f4196c
fdezan <fabio.dezan@gmail.com>
2017-04-17 17:42:08 -0300

    page home com categorias

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :proposals
+  has_many :services
+  has_many :jobs, through: :services, source: :proposals
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
