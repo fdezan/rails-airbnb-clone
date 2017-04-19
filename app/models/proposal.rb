1a754c150ffde0e9cdd363d2807610229f4b4ee8
Leticia Rafael <leticiavanelli@gmail.com>
2017-04-19 16:39:47 -0300

    Finishing of create my_proposal's view

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,6 +1,6 @@
 class Proposal < ApplicationRecord
   belongs_to :user
   belongs_to :service
-  validates :description, presence: true
+  validates :description, :user, :service, presence: true
   has_attachments :photos, maximum: 6
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ApplicationRecord
   CATEGORIES = ["Arquitetura", "Web Design", "Programador Ruby Rails", "Engenheiro Civil", "Categoria 1", "Categoria 2" ]
   belongs_to :user
   has_many :proposals
-  validates :description, presence: true
+  validates :description, :user, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   has_attachments :photos, maximum: 20
 end
