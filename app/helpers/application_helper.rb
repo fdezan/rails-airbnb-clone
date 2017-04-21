5539a9d71d6854e0e3f9bb1430006b51a05e2f54
Leticia Rafael <leticiavanelli@gmail.com>
2017-04-21 11:59:10 -0300

    Fixed helper photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    user.facebook_picture_url || cl_image_path(user.photo.path) || "http://placehold.it/30x30"
+    if user.facebook_picture_url.present?
+      user.facebook_picture_url
+    elsif user.photo.present?
+      cl_image_path(user.photo.path)
+    else
+      "http://placehold.it/30x30"
+    end
   end
 
   def background_url(my_service)
